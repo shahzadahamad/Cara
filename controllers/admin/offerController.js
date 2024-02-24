dafd1f2dde429b0758400b718b3674d10d9f90c8
Shahzad Ahamad P <shahzadahamadp@gmail.com>
2024-02-24 12:59:54 +0530

    offer changed to the added offer can add different product or category

--- controllers/admin/offerController.js
+++ controllers/admin/offerController.js
@@ -1,173 +1,246 @@
-const Category = require('../../models/categoryModel');
-const Product = require('../../models/productsModel');
-const Offer = require('../../models/offerModel');
-const Admin = require('../../models/adminModel');
+const Category = require("../../models/categoryModel");
+const Product = require("../../models/productsModel");
+const Offer = require("../../models/offerModel");
+const Admin = require("../../models/adminModel");
 
-const validation = (offerTitle,offerType,discountPercentage,startDate,endDate,description)=>{
-  
-  if (!offerTitle||!offerType||!discountPercentage||!startDate||!endDate||!description) {
-    return "All Fields are require or Invaled Field" 
+const validation = (
+  offerTitle,
+  offerType,
+  discountPercentage,
+  startDate,
+  endDate,
+  description
+) => {
+  if (
+    !offerTitle ||
+    !offerType ||
+    !discountPercentage ||
+    !startDate ||
+    !endDate ||
+    !description
+  ) {
+    return "All Fields are require or Invaled Field";
   }
 
-
-  if(isNaN(discountPercentage) || discountPercentage<=0){
-    return "Dicount must be a positive number" 
+  if (isNaN(discountPercentage) || discountPercentage <= 0) {
+    return "Dicount must be a positive number";
   }
 
-
-  if(isNaN(discountPercentage) || discountPercentage>100){
-    return "Dicount must be a 0 - 100" 
-  }
-
-  const expiryDataRegex = /^\d{4}-\d{2}-\d{2}$/;
-  if(!expiryDataRegex.test(endDate)||!expiryDataRegex.test(startDate)){
-    return "Invalid expiry date format" 
-  }
-
-  return false
-}
-
-const validationEdit = (editOfferTitle,editDiscountPercentage,editStartDate,editEndDate,editDescription)=>{
-
-
-  if (!editOfferTitle||!editDiscountPercentage||!editStartDate||!editEndDate||!editDescription) {
-    return "All Fields are require or Invaled Field" 
-  }
- 
-  if(isNaN(editDiscountPercentage) || editDiscountPercentage<=0){
-    return "Dicount must be a positive number" 
-  }
-
-
-  if(isNaN(editDiscountPercentage) || editDiscountPercentage>100){
-    return "Dicount must be a 0 - 100" 
+  if (isNaN(discountPercentage) || discountPercentage > 100) {
+    return "Dicount must be a 0 - 100";
   }
 
   const expiryDataRegex = /^\d{4}-\d{2}-\d{2}$/;
-  if(!expiryDataRegex.test(editEndDate)||!expiryDataRegex.test(editStartDate)){
-    return "Invalid expiry date format" 
+  if (!expiryDataRegex.test(endDate) || !expiryDataRegex.test(startDate)) {
+    return "Invalid expiry date format";
   }
 
-  return false
-}
+  return false;
+};
 
-const loadOffers = async (req,res) => {
-  try{
-    const admin = await Admin.findOne({_id:req.session.admin_id})
+const validationEdit = (
+  editOfferTitle,
+  editDiscountPercentage,
+  editStartDate,
+  editEndDate,
+  editDescription
+) => {
+  if (
+    !editOfferTitle ||
+    !editDiscountPercentage ||
+    !editStartDate ||
+    !editEndDate ||
+    !editDescription
+  ) {
+    return "All Fields are require or Invaled Field";
+  }
+
+  if (isNaN(editDiscountPercentage) || editDiscountPercentage <= 0) {
+    return "Dicount must be a positive number";
+  }
+
+  if (isNaN(editDiscountPercentage) || editDiscountPercentage > 100) {
+    return "Dicount must be a 0 - 100";
+  }
+
+  const expiryDataRegex = /^\d{4}-\d{2}-\d{2}$/;
+  if (
+    !expiryDataRegex.test(editEndDate) ||
+    !expiryDataRegex.test(editStartDate)
+  ) {
+    return "Invalid expiry date format";
+  }
+
+  return false;
+};
+
+const loadOffers = async (req, res) => {
+  try {
+    const admin = await Admin.findOne({ _id: req.session.admin_id });
     const offer = await Offer.find();
-    res.render('offer',{admins:admin,offer:offer});
-  }catch(error){
-    console.log(error.message)
+    res.render("offer", { admins: admin, offer: offer });
+  } catch (error) {
+    console.log(error.message);
   }
 };
 
-const getCategoriesAndProduct = async (req,res) => {
-  try{
-    const {value}=req.body;
-    if(value==='category'){
-      const category = await Category.find({},{name:1,_id:0});
-      res.json({category});
-    }else if(value==='product'){
-      const product = await Product.find({},{name:1,_id:0});
-      res.json({product})
-    }
-  }catch(error){
-    console.log(error.message)
-  }
-};
+const verifyOffer = async (req, res) => {
+  try {
+    const {
+      offerTitle,
+      offerType,
+      discountPercentage,
+      startDate,
+      endDate,
+      description,
+    } = req.body;
 
-const verifyOffer = async (req,res) =>{
-  try{
-    const {offerTitle,offerType,category,product,discountPercentage,startDate,endDate,description}=req.body;
-    
-    const validationResult = validation(offerTitle,offerType,discountPercentage,startDate,endDate,description);
+    const validationResult = validation(
+      offerTitle,
+      offerType,
+      discountPercentage,
+      startDate,
+      endDate,
+      description
+    );
 
-    if(validationResult){
-      res.json({message:validationResult})
-    }else{
+    if (validationResult) {
+      res.json({ message: validationResult });
+    } else {
       const offer = new Offer({
-        offerTitle:offerTitle,
-        offerType:offerType,
-        discountPercentage:discountPercentage,
-        description:description,
-        startDate:new Date(startDate).setHours(23,59,59,999),
-        endDate:new Date(endDate).setHours(23,59,59,999),
-        createAt:new Date(),
+        offerTitle: offerTitle,
+        offerType: offerType,
+        discountPercentage: discountPercentage,
+        description: description,
+        startDate: new Date(startDate).setHours(23, 59, 59, 999),
+        endDate: new Date(endDate).setHours(23, 59, 59, 999),
+        createAt: new Date(),
       });
-
-      if(offerType==='category'){
-        await Category.updateOne({name:category},{$set:{offer:offer._id}})
-      }else if(offerType==='product'){
-        await Product.updateOne({name:product},{$set:{offer:offer._id}})  
-      }
       await offer.save();
-      res.json({status:true});
+      res.json({ status: true });
     }
-
-  }catch(error){
+  } catch (error) {
     console.log(error.message);
   }
 };
 
-const deleteOffer = async (req,res) =>{
-  try{
-    const {id}=req.query;
-    await Category.updateMany({offer:id},{$unset:{offer:1}});
-    await Product.updateMany({offer:id},{$unset:{offer:1}});
-    await Offer.deleteOne({_id:id});
-    res.json({status:true});
-  }catch(error){
+const deleteOffer = async (req, res) => {
+  try {
+    const { id } = req.query;
+    await Category.updateMany({ offer: id }, { $unset: { offer: 1 } });
+    await Product.updateMany({ offer: id }, { $unset: { offer: 1 } });
+    await Offer.deleteOne({ _id: id });
+    res.json({ status: true });
+  } catch (error) {
     console.log(error.message);
   }
 };
 
-const editOffer = async (req,res) => {
-  try{
-    const {id}=req.body;
-    const offer = await Offer.findOne({_id:id});
-    res.json({data:offer});
-  }catch(error){
-    console.log(error.message)
+const editOffer = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const offer = await Offer.findOne({ _id: id });
+    res.json({ data: offer });
+  } catch (error) {
+    console.log(error.message);
   }
 };
 
-const verifyEditOffer = async (req,res) => {
-  try{
-    const {id}=req.body;
-    const {editOfferTitle,editDiscountPercentage,editStartDate,editEndDate,editDescription} = req.body.jsonData;
-    const validationResult = validationEdit(editOfferTitle,editDiscountPercentage,editStartDate,editEndDate,editDescription);
+const verifyEditOffer = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const {
+      editOfferTitle,
+      editDiscountPercentage,
+      editStartDate,
+      editEndDate,
+      editDescription,
+    } = req.body.jsonData;
+    const validationResult = validationEdit(
+      editOfferTitle,
+      editDiscountPercentage,
+      editStartDate,
+      editEndDate,
+      editDescription
+    );
 
-    if(validationResult){
-      res.json({message:validationResult});
-    }else{
-      const offer = await Offer.findOne({_id:id});
+    if (validationResult) {
+      res.json({ message: validationResult });
+    } else {
+      const offer = await Offer.findOne({ _id: id });
 
-      if(offer.offerTitle==editOfferTitle&&offer.discountPercentage==editDiscountPercentage&&offer.startDate.toDateString()==new Date(editStartDate).toDateString()&&offer.endDate.toDateString()==new Date(editEndDate).toDateString()&&offer.description==editDescription){
-        return res.json({message:'No Changes Made'});
+      if (
+        offer.offerTitle == editOfferTitle &&
+        offer.discountPercentage == editDiscountPercentage &&
+        offer.startDate.toDateString() ==
+          new Date(editStartDate).toDateString() &&
+        offer.endDate.toDateString() == new Date(editEndDate).toDateString() &&
+        offer.description == editDescription
+      ) {
+        return res.json({ message: "No Changes Made" });
       }
 
       const update = {
-        offerTitle:editOfferTitle,
-        discountPercentage:editDiscountPercentage,
-        startDate:new Date(editStartDate).setHours(23,59,59,999),
-        endDate:new Date(editEndDate).setHours(23,59,59,999),
-        description:editDescription,
-      }
+        offerTitle: editOfferTitle,
+        discountPercentage: editDiscountPercentage,
+        startDate: new Date(editStartDate).setHours(23, 59, 59, 999),
+        endDate: new Date(editEndDate).setHours(23, 59, 59, 999),
+        description: editDescription,
+      };
 
-      await Offer.updateOne({_id:id},{$set:update});
-      res.json({status:true});
+      await Offer.updateOne({ _id: id }, { $set: update });
+      res.json({ status: true });
     }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
+const loadChooseOffer = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const adminData = await Admin.findById({ _id: req.session.admin_id });
+    const offer = await Offer.findOne({ _id: id });
+    if (offer.offerType === "product") {
+      const products = await Product.find();
+      res.render("offerProducts", { admins: adminData, product: products,id:id });
+    }else if(offer.offerType==='category'){
+      const category = await Category.find();
+      res.render("offerProducts", { admins: adminData, category: category,id:id });
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const verifyAddingOffer = async (req,res) => {
+  try{
+    const {_id,type,id,checkbox}=req.body;
+    if(type==='category'){
+      if(checkbox){
+        await Category.updateOne({_id:_id},{$set:{offer:id}})
+      }else{
+        await Category.updateOne({_id:_id},{$unset:{offer:id}})
+      }
+    }else if(type==='product'){
+      if(checkbox){
+        await Product.updateOne({_id:_id},{$set:{offer:id}})
+      }else{
+        await Product.updateOne({_id:_id},{$unset:{offer:id}})
+      }
+    }
+    res.json({status:'success'});
   }catch(error){
     console.log(error.message);
   }
 }
- 
+
 module.exports = {
   loadOffers,
-  getCategoriesAndProduct,
   verifyOffer,
   deleteOffer,
   editOffer,
   verifyEditOffer,
-}
+  loadChooseOffer,
+  verifyAddingOffer,
+};
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,30 +3,24 @@ const adminRouter = express();
 const auth = require('../middleware/adminAuth');
 const upload = require('../middleware/multer');
 
+// view engine
+adminRouter.set('view engine','ejs');
+adminRouter.set('views','./views/admin');
+
 // <--------------Admin Controller-------------->
 const adminController = require('../controllers/admin/adminController');
 // <--------------Admin Controller-------------->
 
-// viewEngine
-adminRouter.set('view engine','ejs');
-adminRouter.set('views','./views/admin');
-
-// login
 adminRouter.get('/',auth.isLogout,adminController.loadLogin);
 adminRouter.post('/login',auth.isLogout,adminController.verifyLogin);
-
-// logout
 adminRouter.get('/logout',auth.isLogin,adminController.adminLogout);
 
 // <--------------Dashboard Controller-------------->
 const dashboardController = require('../controllers/admin/dashboardController');
 // <--------------Dashboard Controller-------------->
 
-// dashboard
 adminRouter.get('/dashboard',auth.isLogin,dashboardController.loadDashboard);
 adminRouter.patch('/dashboard',dashboardController.verifyDashboard);
-
-// custum sales report
 adminRouter.get('/custom-sale-report',auth.isLogin,dashboardController.loadCustomSalesReport);
 adminRouter.post('/custom-sale-report',dashboardController.verifyCustomSalesReport);
 
@@ -34,78 +28,48 @@ adminRouter.post('/custom-sale-report',dashboardController.verifyCustomSalesRepo
 const categoryController = require('../controllers/admin/categoryController');
 // <--------------Category Controller-------------->
 
-// category
 adminRouter.get('/category',auth.isLogin,categoryController.loadCategory);
-
-// addCategory
 adminRouter.get('/add-category',auth.isLogin,categoryController.loadAddCategory);
 adminRouter.post('/add-category',categoryController.verifyAddCategory);
-
-// editCategoy
 adminRouter.get('/edit-categorys',auth.isLogin,categoryController.loadEditCategory);
 adminRouter.post('/edit-categorys',categoryController.verifyEditCategory);
-
-// deleteCategory
 adminRouter.delete('/delete-category',categoryController.deleteCategory);
 
 // <--------------Product Controller-------------->
 const productController = require('../controllers/admin/productController');
 // <--------------Product Controller-------------->
 
-// products
 adminRouter.get('/products',auth.isLogin,productController.loadProducts);
-
-// addProducts
 adminRouter.get('/add-products',auth.isLogin,productController.loadAddProducts);
 adminRouter.post('/add-products',upload.array('img',4),productController.verifyAddProducts);
-
-// editProduct
 adminRouter.get('/edit-products',auth.isLogin,productController.loadEditProduct);
 adminRouter.post('/edit-products',upload.array('img',4),productController.verifyEditProduct);
-
-//edit Image
 adminRouter.patch('/edit-images',productController.verifyEditImage)
-
-// deleteProduct
 adminRouter.delete('/delete-product',productController.verifyDeleteProduct);
 
 // <--------------Order Controller-------------->
 const orderController = require('../controllers/admin/orderController');
 // <--------------Order Controller-------------->
 
-// order 
 adminRouter.get('/order-detials',auth.isLogin,orderController.loadOrderDetials);
-
-// order full detials 
 adminRouter.get('/order-full-detials',auth.isLogin,orderController.loadOrderFullDetials);
-
-// order status chaning
 adminRouter.patch('/order-status',orderController.editOrderStatus);
 
 // <--------------Order Controller-------------->
 const userController = require('../controllers/admin/userController');
 // <--------------Order Controller-------------->
 
-// userDetials
 adminRouter.get('/user-detials',auth.isLogin,userController.loadUserDetials);
-
-// block
 adminRouter.patch('/block-user',userController.verifyBlockUser);
 
 // <--------------Coupon Controller-------------->
 const couponController = require('../controllers/admin/couponController');
 // <--------------Coupon Controller-------------->
 
-
-//add coupon
 adminRouter.get('/coupon',auth.isLogin,couponController.loadCouponDetials);
 adminRouter.post('/add-coupon',couponController.verifyAddCoupon);
-
-//edit coupon
 adminRouter.patch('/edit-coupon',couponController.verifyEditCoupon);
 adminRouter.patch('/edit-coupon-confirm',couponController.verifyEditCouponConfirm);
-
-// delete coupon 
 adminRouter.delete('/coupon-delete',couponController.deleteCoupon);
 
 // <--------------Offer Controller-------------->
@@ -113,11 +77,12 @@ const offerController = require('../controllers/admin/offerController');
 // <--------------Offer Controller-------------->
 
 adminRouter.get('/offer',auth.isLogin,offerController.loadOffers);
-adminRouter.patch('/offer-category',auth.isLogin,offerController.getCategoriesAndProduct);
 adminRouter.post('/add-offer',offerController.verifyOffer);
 adminRouter.delete('/offer-delete',offerController.deleteOffer);
 adminRouter.patch('/edit-offer',offerController.editOffer);
 adminRouter.patch('/edit-offer-confirm',offerController.verifyEditOffer);
+adminRouter.get('/choose-offer',auth.isLogin,offerController.loadChooseOffer);
+adminRouter.patch('/adding-offers',auth.isLogin,offerController.verifyAddingOffer);
 
 
 module.exports = adminRouter;
